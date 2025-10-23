51a7b47ae98d399ada7157cde0dfe98802f02fcb
Nick Rodriguez <nrodrig1@gmail.com>
2025-10-23 14:40:22 -0700

    mod: color pallete, added some contracts - less black

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -25,7 +25,7 @@ export default function DashboardLayout({
 				className={`flex-1 transition-all duration-300 ease-in-out ${mainContentMargin}`}
 			>
 				<AppHeader />
-				<div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+				<div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6 dark:bg-gray-950">
 					{children}
 				</div>
 			</div>
--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -278,7 +278,7 @@ const AppSidebar: React.FC = () => {
 
 	return (
 		<aside
-			className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 right-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-[100000] border-l border-gray-200
+			className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 right-0 bg-white dark:bg-gray-800 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-[100000] border-l border-gray-200
         ${isExpanded || isMobileOpen ? "w-[290px]" : "w-[90px] cursor-pointer"}
         ${isMobileOpen ? "translate-x-0" : "translate-x-full"}
         lg:translate-x-0`}
